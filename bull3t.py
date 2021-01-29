90e2c204dd050a986c7891a3ac3316d63d58df20
ahmad-shakla <66517841+ahmad-shakla@users.noreply.github.com>
2021-01-29 00:26:55 +0000

    Create bull3t.py

---
+++ bull3t.py
@@ -0,0 +1,47 @@
+import os
+def banner():
+        
+        print ("""
+        
+        
+               Targets are located , guns are reloaded
+                    
+ ______________|______________        
+ |             |             |
+ |             |             |
+ |             |             |
+ |             |             |
+_|_________         _________|_
+ |                           |
+ |             |             |
+ |             |             |
+ |             |             |
+ |_____________|_____________|        
+               |                Created by ahmad shakla
+
+=======>>   0.Exit
+=======>>   1.Create an encrypted metasploit windows payload
+=======>>   2.Create a php payload
+=======>>   3.Create a python payload
+=======>>   4.Create a shell-script payload
+=======>>   5.Create payloads injected with images
+=======>>   6.Create encrypted payoads injected with images 
+        
+        
+        """)
+        trigger = input("TRIGGER >>")
+
+       
+def install():
+
+        print("installing tools please wait...\n\n\")
+        os.system("apt-get update")
+        os.system("clear")
+        os.system("apt-get install exiftool")
+        os.system("clear")
+        os.system("apt-get install exe2hex")
+        os.system("clear")
+        
+        
+install()
+banner()
